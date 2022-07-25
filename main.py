a3df158ef5ddf4254d0d271a600e7aa0e0f6ea22
Jawdat Abdullah abdallh <>
2022-07-25 20:47:15 +0000

    adding first html file

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)  # Flask("main.py)
 
 
 @app.route("/")  # Create a page on the first url
 def homePage():
-    return "Hello, World!"
+    return render_template("homepage.html")
 
 
 app.run(host="0.0.0.0")
