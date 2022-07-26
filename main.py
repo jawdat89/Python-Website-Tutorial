44a1dac396be452597b43b1b6e1bec10ffe03308
Jawdat Abdullah abdallh <>
2022-07-26 15:36:58 +0000

    add router

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-from flask import Flask, render_template
+from program import create_app
 
-app = Flask(__name__, static_url_path="", static_folder="static")  # Flask("main.py)
-
-
-@app.route("/")  # Create a page on the first url
-def homePage():
-    items = [
-     "1234",
-      "2345",
-      "5678"
-    ];
-    return render_template("homepage.html", todolist = items)
-
-
-app.run(host="0.0.0.0")
+create_app().run(host="0.0.0.0")
---
+++ mainroutes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+routes = Blueprint('routes', __name__)
+
+@routes.route("/")  # Create a page on the first url
+def homePage():
+  items = [
+       "1234",
+        "2345",
+        "5678"
+      ];
+  return render_template("homepage.html", todolist = items)
